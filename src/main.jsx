3840bbbafcbe15fd81d67f1087d74006116da962
Jems <jose.e.molina2013@gmail.com>
2025-06-06 15:20:41 -0600

    borrar archivo test1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Test1 from './pages/test1.jsx'
 import Header from './components/header.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
